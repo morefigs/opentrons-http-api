508c74bf15c95d6cd9ff998f1cbc93490ccdae40
morefigs <morefigs@gmail.com>
2023-12-20 18:20:32 +1100

    Reference functions directly

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Callable
 from unittest.mock import Mock, patch
 
 import pytest
@@ -101,71 +101,80 @@ def test_post(api):
             assert response == mock_response.json()
 
 
-@pytest.mark.parametrize('method_name, path, kwargs', [
-    ('get_robot_lights', Paths.ROBOT_LIGHTS, {}),
-    ('get_settings', Paths.SETTINGS, {}),
-    ('get_robot_settings', Paths.SETTINGS_ROBOT, {}),
-    ('get_calibration_status', Paths.CALIBRATION_STATUS, {}),
-    ('get_motors_engaged', Paths.MOTORS_ENGAGED, {}),
-    ('get_health', Paths.HEALTH, {}),
-    ('get_runs', Paths.RUNS, {}),
-    ('get_runs_run_id', Paths.RUNS_RUN_ID, {'run_id': 'run_123'}),
-    ('get_runs_run_id_commands', Paths.RUNS_RUN_ID_COMMANDS, {'run_id': 'run_123'}),
-    ('get_runs_run_id_commands_command_id', Paths.RUNS_RUN_ID_COMMANDS_COMMAND_ID, {'run_id': 'run_123',
-                                                                                    'command_id': 'command_123'}),
-    ('get_protocols', Paths.PROTOCOLS, {}),
-    ('get_protocols_protocol_id', Paths.PROTOCOLS_PROTOCOL_ID, {'protocol_id': 'protocol_123'}),
+@pytest.mark.parametrize('method, path, path_kwargs', [
+    (API.get_robot_lights, Paths.ROBOT_LIGHTS, {}),
+    (API.get_settings, Paths.SETTINGS, {}),
+    (API.get_robot_settings, Paths.SETTINGS_ROBOT, {}),
+    (API.get_calibration_status, Paths.CALIBRATION_STATUS, {}),
+    (API.get_motors_engaged, Paths.MOTORS_ENGAGED, {}),
+    (API.get_health, Paths.HEALTH, {}),
+    (API.get_runs, Paths.RUNS, {}),
+    (API.get_runs_run_id, Paths.RUNS_RUN_ID, {'run_id': 'run_123'}),
+    (API.get_runs_run_id_commands, Paths.RUNS_RUN_ID_COMMANDS, {'run_id': 'run_123'}),
+    (API.get_runs_run_id_commands_command_id, Paths.RUNS_RUN_ID_COMMANDS_COMMAND_ID, {'run_id': 'run_123',
+                                                                                      'command_id': 'command_123'}),
+    (API.get_protocols, Paths.PROTOCOLS, {}),
+    (API.get_protocols_protocol_id, Paths.PROTOCOLS_PROTOCOL_ID, {'protocol_id': 'protocol_123'}),
 ])
-def test_get_methods(api_with_mock_get: API, method_name: str, path: str, kwargs: Dict):
+def test_get_methods(api_with_mock_get: API, method: Callable, path: str, path_kwargs: Dict):
     """
     Tests the API's get_* methods by mocking API._get.
     """
-    assert getattr(api_with_mock_get, method_name)(**kwargs) == RESPONSE
-    api_with_mock_get._get.assert_called_once_with(path.format(**kwargs))
+    # Get a reference to the same method but on the mocked object
+    function = getattr(api_with_mock_get, method.__name__)
+
+    assert function(**path_kwargs) == RESPONSE
+
+    path = path.format(**path_kwargs)
+    api_with_mock_get._get.assert_called_once_with(path)
 
 
-@pytest.mark.parametrize('method_name, path, path_kwargs, kwargs_in, kwargs_out', [
+@pytest.mark.parametrize('method, path, path_kwargs, kwargs_in, kwargs_out', [
     (
-            'post_identify', Paths.IDENTIFY, {},
+            API.post_identify, Paths.IDENTIFY, {},
             {'seconds': 3},
             {'query': {'seconds': 3}},
     ),
     (
-            'post_robot_lights', Paths.ROBOT_LIGHTS, {},
+            API.post_robot_lights, Paths.ROBOT_LIGHTS, {},
             {'on': True},
             {'body': {'on': True}},
     ),
     (
-            'post_settings', Paths.SETTINGS, {},
+            API.post_settings, Paths.SETTINGS, {},
             {'id_': SettingId.SHORT_FIXED_TRASH, 'value': True},
             {'body': {'id': SettingId.SHORT_FIXED_TRASH, 'value': True}},
     ),
     (
-            'post_motors_disengage', Paths.MOTORS_DISENGAGE, {},
+            API.post_motors_disengage, Paths.MOTORS_DISENGAGE, {},
             {'axes': [Axis.X, Axis.Y]},
             {'body': {'axes': [Axis.X, Axis.Y]}},
     ),
     (
-            'post_runs', Paths.RUNS, {'protocol_id': 'protocol_123'},
+            API.post_runs, Paths.RUNS, {'protocol_id': 'protocol_123'},
             {'labware_offsets': [{'labware': 'offsets'}]},
             {'body': {'data': {'protocolId': 'protocol_123', 'labwareOffsets': [{'labware': 'offsets'}]}}},
     ),
     (
-            'post_runs_run_id_actions', Paths.RUNS_RUN_ID_ACTIONS, {'run_id': 'run_123'},
+            API.post_runs_run_id_actions, Paths.RUNS_RUN_ID_ACTIONS, {'run_id': 'run_123'},
             {'action': ActionType.PLAY},
             {'body': {'data': {'actionType': ActionType.PLAY}}},
     ),
     (
-            'post_protocols', Paths.PROTOCOLS, {},
+            API.post_protocols, Paths.PROTOCOLS, {},
             {'protocol_file': b'file_contents'},
             {'files': [('files', b'file_contents')]},
     ),
 ])
-def test_post_methods(api_with_mock_post: API, method_name: str, path: str, path_kwargs: Dict, kwargs_in: Dict,
+def test_post_methods(api_with_mock_post: API, method: Callable, path: str, path_kwargs: Dict, kwargs_in: Dict,
                       kwargs_out: Dict):
     """
     Tests the API's post_* methods by mocking API._post.
     """
+    # Get a reference to the same method but on the mocked object
+    function = getattr(api_with_mock_post, method.__name__)
+
+    assert function(**path_kwargs, **kwargs_in) == RESPONSE
+
     path = path.format(**path_kwargs)
-    assert getattr(api_with_mock_post, method_name)(**path_kwargs, **kwargs_in) == RESPONSE
     api_with_mock_post._post.assert_called_once_with(path, **kwargs_out)
