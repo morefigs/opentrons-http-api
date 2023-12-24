1d8e1d82643ac38ab05daa79551e45e8a3663733
morefigs <morefigs@gmail.com>
2023-12-24 14:04:37 +1100

    Added robot funcs

--- opentrons_http_api/robot.py
+++ opentrons_http_api/robot.py
@@ -1,71 +1,54 @@
 from __future__ import annotations
-from typing import Dict, List, Tuple, BinaryIO, Optional, Sequence
-from dataclasses import dataclass
-from abc import ABC
+from typing import Dict, Tuple, BinaryIO, Optional, Sequence
 
 from opentrons_http_api.api import API, SettingId, ActionType
+from opentrons_http_api.robot_info import SettingsInfo, RobotSettingsInfo, HealthInfo, RunInfo, ProtocolInfo
 
 
-class _Info(ABC):
-    """
-    Base info class.
-    """
-    def __init__(self, args, kwargs):
-        pass
+class Robot:
+    def __init__(self, host: str = 'localhost'):
+        self._api = API(host)
 
-    @classmethod
-    def from_dict(cls, d: dict) -> _Info:
-        return cls(**d)
+    def identify(self, seconds: int) -> None:
+        self._api.post_identify(seconds)
 
+    def lights(self) -> bool:
+        return self._api.get_robot_lights()['on']
 
-@dataclass(frozen=True)
-class SettingInfo(_Info):
-    id: str
-    old_id: str
-    title: str
-    description: str
-    restart_required: bool
-    value: bool
+    def set_lights(self, on: bool) -> None:
+        self._api.post_robot_lights(on)
 
+    def settings(self) -> Tuple[SettingsInfo, ...]:
+        d = self._api.get_settings()
+        return tuple(SettingsInfo.from_dict(setting)
+                     for setting in d['settings'])
 
-@dataclass(frozen=True)
-class HealthInfo(_Info):
-    name: str
-    robot_model: str
-    api_version: str
-    fw_version: str
-    board_revision: str
-    logs: List[str]
-    system_version: str
-    maximum_protocol_api_version: List[int]
-    minimum_protocol_api_version: List[int]
-    robot_serial: str
-    links: Dict[str, str]
+    def set_setting(self, id_: SettingId, value: bool) -> None:
+        self._api.post_settings(id_.value, value)
 
+    def robot_settings(self) -> RobotSettingsInfo:
+        d = self._api.get_robot_settings()
+        return RobotSettingsInfo.from_dict(d)
 
-@dataclass(frozen=True)
-class RunInfo(_Info):
-    id: str
-    createdAt: str
-    status: str
-    current: bool
-    actions: List[dict]
-    errors: List[dict]
-    pipettes: List[dict]
-    modules: List[dict]
-    labware: List[dict]
-    liquids: List[dict]
-    labwareOffsets: List[dict]
-    protocolId: str
+    def health(self) -> HealthInfo:
+        info = self._api.get_health()
+        return HealthInfo.from_dict(info)
 
+    def create_run(self, protocol_id: str, labware_offsets: Optional[Sequence[dict]] = None) -> RunInfo:
+        d = self._api.post_runs(protocol_id, labware_offsets)
+        return RunInfo.from_dict(d['data'])
 
-@dataclass(frozen=True)
-class ProtocolInfo(_Info):
-    id: str
-    createdAt: str
-    files: List[Dict]
-    protocolType: str
-    robotType: str
-    metadata: Dict
-    analyses: List
-    analysisSummaries: List[Dict]
+    def action_run(self, run_id: str, action: ActionType) -> None:
+        self._api.post_runs_run_id_actions(run_id, action)
+
+    def upload_protocol(self, protocol_file: BinaryIO,
+                        labware_definitions: Optional[Sequence[BinaryIO]] = None) -> ProtocolInfo:
+        """
+        Upload a protocol with optional labware definitions to the robot.
+        :param protocol_file: A Python or JSON protocol binary file object.
+        :param labware_definitions: An optional sequence of JSON labware definition binary file objects, only if the
+        protocol_file is in Python format.
+        :return: ProtocolInfo object containing information about the protocol.
+        """
+        d = self._api.post_protocols(protocol_file, labware_definitions)
+        return ProtocolInfo.from_dict(d['data'])
